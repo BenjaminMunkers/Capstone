cd4b62fff3c08e201bbc35dc5132573305fe430f
Ben Munkers <benjamin.munkers@gmail.com>
2023-02-28 10:14:44 -0700

    Added a graph to look deeper into the numbers and their spread. Looks like the yields may already be in yield per acre.

--- PreliminaryCode.R
+++ PreliminaryCode.R
@@ -4,6 +4,7 @@ df1 = read.csv("C:/Users/jammu/Desktop/EFIN 499/msuCapstone_countyYields/UnitYie
 df1$yieldAcre = df1$yield/df1$acres
 state18 = subset(df1, stateFips == 18)
 state18 = subset(state18, commodityCode == 41)
+state18 = subset(state18, cropYear == 2016)
 state17 = subset(df1, stateFips ==17)
 county18 = data.frame(unique(state18$countyFips))
 county18$meanYield = 1
@@ -22,5 +23,9 @@ for(i in unique(state18$countyFips)){
   county18$meanYA[count] = mean(tempdf$yieldAcre)
   county18$minYA[count] = min(tempdf$yieldAcre)
   county18$maxYA[count] = max(tempdf$yieldAcre)
-  
 }
+tempdf = subset(state18, countyFips == 13) #13 has one of the lowest number of 
+#farms. Other options are 25, 97, 87, 43, and 29.
+plot(x = tempdf$yield, y = tempdf$acres, main = "Yield vs. Acres", xlab = "Yield", 
+     ylab = "Acres")
+table(state18$countyFips)
