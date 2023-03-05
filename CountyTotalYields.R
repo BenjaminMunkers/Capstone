8b29f95a35d9eaa7f3a6296707a8d0b96b01ab30
JosephPauly <76223893+JosephPauly@users.noreply.github.com>
2023-03-05 15:10:31 -0800

    Add files via upload
    Added chunk of code that averages the weekly drought levels on a yearly basis

--- CountyTotalYields.R
+++ CountyTotalYields.R
@@ -2,8 +2,9 @@
 #Setting up a DF where we can see county yield by year.
 rm(list = ls())
 library(dplyr)
+library(tidyr)
 # import data
-df1 = read.csv("C:/Users/jammu/Desktop/EFIN 499/msuCapstone_countyYields/UnitYields.csv")
+df1 = read.csv("C:/Users/Joseph/Desktop/EFIN 499/msuCapstone_countyYields/msuCapstone_countyYields/UnitYields.csv")
 # Gets the years of the data frame
 years = unique(df1$cropYear)
 years = sort(years)
@@ -32,7 +33,7 @@ counties = averageYields
 for(i in 1:length(years)){
   temp = subset(tempdf, cropYear == years[i])
   #farms = temp %>% 
-    #count(countyFips)
+  #count(countyFips)
   for(j in 1:length(counties)){
     newtemp = subset(temp, countyFips == counties[j])
     yield = sum(newtemp$yield * newtemp$acres)
@@ -41,3 +42,29 @@ for(i in 1:length(years)){
     #counties[j, i+1] = farms[j,2]
   }
 }
+
+# read in county names
+county.names.df = read.csv("C:/Users/Joseph/Desktop/EFIN 499/msuCapstone_countyYields/CountyNames.csv")
+
+# read in drought data 
+drought.df = read.csv("C:/Users/Joseph/Desktop/EFIN 499/dm_export_19970201_20170201.csv")
+
+# extract year from each entry
+library(lubridate)
+drought.df$date <- as.Date(drought.df$ValidStart)
+drought.df$year <- year(drought.df$ValidStart)
+
+# average weekly percentage of drought levels to yearly data
+average.drought.levels <- 
+  aggregate(
+    cbind(drought.df$None, drought.df$D0, drought.df$D1, drought.df$D2, drought.df$D3, drought.df$D4), 
+    list(drought.df$FIPS, drought.df$year),
+            data = drought.df, FUN = mean)
+
+# rename columns
+colnames(average.drought.levels) = c('FIPS', 'Year', 'avg.None', 'avg.D0', 'avg.D1', 'avg.D2', 'avg.D3', 'avg.D4')
+
+# merge yearly averaged data with main drought dataset
+drought.aggregated.df = left_join(average.drought.levels, drought.df, multiple = "all")
+
+write.csv(drought.aggregated.df, file = 'C:/Users/Joseph/Desktop/EFIN 499/aggregated_drought_data.csv')
