f7817f68797fed9f67d3c970be2b75704678cb26
JosephPauly <76223893+JosephPauly@users.noreply.github.com>
2023-04-03 07:30:28 -0800

    Add files via upload
    Merged drought variables with the main county yields dataset

--- Weeks of Drought.R
+++ Weeks of Drought.R
@@ -18,7 +18,8 @@ drought.df$year <- year(drought.df$MapDate)
 # Aggregate the data to number of weeks each year that are in each drought level
 drought_aggregated <- drought.df |>
   group_by(year,County) |>
-  summarize(num_none = sum(None),
+  summarize(FIPS = FIPS,
+            num_none = sum(None),
             num_d0 = sum(D0),
             num_d1 = sum(D1),
             num_d2 = sum(D2),
@@ -37,3 +38,28 @@ drought_aggregated <- drought.df |>
 print(drought_aggregated)
 test = subset(drought.df, year == 2000 & County == "Adams County")
 (sum(test$None) / sum(test$None + test$D0 + test$D1 + test$D2 + test$D3 + test$D4))*52
+
+drought_aggregated1 = unique(drought_aggregated)
+
+################################################################################
+
+# merge with weekly averaged drought level dataframe
+library(tictoc)
+
+county_merged_data = read.csv("C:/Users/Joseph/Desktop/EFIN 499/county_merged_drought_data.csv")
+colnames(drought_aggregated1)[1] = "cropYear"
+tic()
+county_yield_drought_data <- merge(county_merged_data, drought_aggregated1, by = c("cropYear", "FIPS"))
+toc()
+
+################################################################################
+
+# check to make sure sum of weeks in drought equals 52
+for (i in 1:nrow(drought_aggregated1)) {
+  drought_aggregated1$weeksinyear[i] = drought_aggregated1$num_none[i]+ drought_aggregated1$num_d0[i]+ drought_aggregated1$num_d1[i]+ 
+    drought_aggregated1$num_d2[i]+ drought_aggregated1$num_d3[i]+ drought_aggregated1$num_d4[i]
+}
+summary(drought_aggregated1$weeksinyear)
+
+################################################################################
+write.csv(drought_aggregated1,"C:/Users/Joseph/Desktop/EFIN 499/main_county_yield_drought.csv")
