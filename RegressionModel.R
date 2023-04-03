4ff8506167048f225107e021abee07170f36f6d2
Ben Munkers <benjamin.munkers@gmail.com>
2023-04-03 09:05:41 -0600

    Added drought averages

--- RegressionModel.R
+++ RegressionModel.R
@@ -10,13 +10,19 @@ for(i in unique(state18$cropYear)){
   for(j in unique(state18$CountyCode)){
     count = count + 1
     countyLevel = subset(state18, CountyCode == j & cropYear == i)
-    countyYield = sum(countyLevel$yield * countyLevel$acres)
+    countyYield = sum(countyLevel$yield * countyLevel$acres)/sum(countyLevel$acres)
     if(count == 1){
-      TYdf = data.frame(cropYear = c(i), CountyCode = c(j), Yield = c(countyYield))
+      TYdf = data.frame(cropYear = c(i), CountyCode = c(j), Yield = c(countyYield), None = countyLevel$avg.None[1], 
+                        D0 = countyLevel$avg.D0[1], D1 = countyLevel$avg.D1[1], D2 = countyLevel$avg.D2[1], D3 = countyLevel$avg.D3[1]
+                        ,D4 = countyLevel$avg.D4[1])
     }
     else{
       TYdf = TYdf %>% 
-        add_row(cropYear = c(i), CountyCode = c(j), Yield = c(countyYield))
+        add_row(cropYear = c(i), CountyCode = c(j), Yield = c(countyYield), None = countyLevel$avg.None[1], 
+                D0 = countyLevel$avg.D0[1], D1 = countyLevel$avg.D1[1], D2 = countyLevel$avg.D2[1], D3 = countyLevel$avg.D3[1]
+                ,D4 = countyLevel$avg.D4[1])
     }
   }
 }
+trainSet = subset(TYdf, CountyCode == 41)
+lm()
