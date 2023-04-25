afda69c9cea68f7959ae35fb86d2e54b9dc8c644
JosephPauly <76223893+JosephPauly@users.noreply.github.com>
2023-04-24 18:29:40 -0800

    Used yield/acre to create weighted confidence intervals

--- Weighted Model.R
+++ Weighted Model.R
@@ -1,7 +1,8 @@
 library(tidyverse)
 library(tictoc)
 library(matrixStats)
-
+library(ggplot2)
+library(dplyr)
 # Read in data
 main_df <- read.csv("C:/Users/Joseph/Desktop/EFIN 499/county_merged_drought_data.csv")
 
@@ -13,6 +14,7 @@ weighted_results_df <- data.frame(Crop = integer(),
                          Year = integer(),
                          County = integer(),
                          Percent_Reported = integer(),
+                         Farms_reported = integer(),
                          Mean_Yield = numeric(),
                          SD_Yield = numeric(),
                          Lower_CI = numeric(),
@@ -44,9 +46,8 @@ for (crop in unique(main_df$commodityCode)) {
         # Perform a random sample of the yields and acres
         sampled_data <- subset_data[sample(nrow(subset_data), n), ]
         # Calculate the weighted mean and standard deviation of the sampled yields
-        yield <- sampled_data$yield * sampled_data$acres
-        mean_yield <- weighted.mean(yield, sampled_data$acres)
-        sd_yield <- sqrt(weightedVar(yield, sampled_data$acres))
+        mean_yield <- weighted.mean(sampled_data$yield, sampled_data$acres)
+        sd_yield <- sqrt(weightedVar(sampled_data$yield, sampled_data$acres))
         # Calculate the confidence interval based on the t-distribution
         t_value <- qt(conf_level, df = n - 1)
         me <- t_value * (sd_yield / sqrt(n))
@@ -55,6 +56,7 @@ for (crop in unique(main_df$commodityCode)) {
                                                    Year = year,
                                                    County = county,
                                                    Percent_Reported = i,
+                                                   Farms_reported = n,
                                                    Mean_Yield = mean_yield,
                                                    Lower_CI = mean_yield - me,
                                                    Upper_CI = mean_yield + me,
@@ -66,21 +68,19 @@ for (crop in unique(main_df$commodityCode)) {
   }
 }
 toc() # 19492.12 sec elapsed
+# 12396.81 sec elapsed after reverting to yield per acre
+
+# Add county names to data frame
+county_names <- main_df |> select(FIPS, CountyName) |> unique()
+names(county_names)[1] <- "County"
+weighted_results_df <- merge(weighted_results_df, county_names, by = "County")
+
+# Save data frame
 write.csv(weighted_results_df, "C:/Users/Joseph/Desktop/EFIN 499/weighted_mean_variance_ci.csv")
+weighted_results_df <- read.csv("C:/Users/Joseph/Desktop/EFIN 499/weighted_mean_variance_ci.csv")
 
 # Graph corn results
-library(ggplot2)
-weighted_results_df |> filter(Crop == 41 & Year == 2016 & County == 17007) |>
-  ggplot(aes(x = Percent_Reported, y = Mean_Yield)) + geom_line(aes(y = Mean_Yield)) +
-  geom_smooth(aes(y = Lower_CI, color = 'Lower 99% CI')) +
-  geom_smooth(aes(y = Upper_CI, color = 'Upper 99% CI'))
-
-
 counties <- unique(weighted_results_df$County)[1:9]
-
-test <- main_df |> select(FIPS, CountyName) |> unique()
-test$County <- test$FIPS
-weighted_results_df <- merge(weighted_results_df, test, by = "County")
 
 weighted_results_df %>%
   filter(Crop == 41 & Year == 2016 & County %in% counties) %>%
